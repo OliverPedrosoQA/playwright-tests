5c656896379c179cc500236a7188b8a942fe0240
Pedroso <oliver.pedroso@vetta.digital>
2024-03-05 09:17:14 -0300

    Added SpecFlow configurationD

---
+++ PlaywrightTests/Drivers/Driver.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace PlaywrightTests.Drivers
+{
+    public class Driver
+    {
+
+    }
+}
---
+++ PlaywrightTests/Hooks/Hook.cs
@@ -0,0 +1,11 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace PlaywrightTests.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+        
+    }
+}
